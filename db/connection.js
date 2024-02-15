b123caaf23173bbe95861f463b75eb3f0ba52696
devang0609 <157216436+devang0609@users.noreply.github.com>
2024-02-15 11:35:17 +0530

    sqlconnect

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,23 @@
 // connection.js
 const mysql = require('mysql');
 const { DB_HOST, DB_USER, DB_PASSWORD, DATABASE, DB_PORT } = require("../config/envConfig");
+const pg = require('pg');
 
-const db = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DATABASE,
-    port: DB_PORT
+// const db = mysql.createConnection({
+//     // host: DB_HOST,
+//     // user: DB_USER,
+//     // password: DB_PASSWORD,
+//     // database: DATABASE,
+//     // port: DB_PORT
+// });
+
+const { Pool } = pg;
+
+const db = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+    connectionTimeoutMillis: 5000 // Adjust the timeout value as needed
 });
+
 
 db.connect((err) => {
     if (err) {
